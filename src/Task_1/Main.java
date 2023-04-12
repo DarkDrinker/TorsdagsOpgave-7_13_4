3b6ce3665bf207979874bd7ab2f147811594bdee
DarkDrinker <Anders.wade.jensen@hotmail.com>
2023-04-12 18:42:29 +0200

    First edit

--- src/Task_1/Main.java
+++ src/Task_1/Main.java
@@ -8,6 +8,13 @@ public class Main {
         tv1.setChannel(30);
         tv1.setVolume(3);
         tv1.printall();
+
+        TV tv2 = new TV();
+        tv2.channelUp();
+        tv2.channelUp();
+        tv2.turnOn();
+        tv2.volumeUp();
+        tv2.printall();
     }
 
 }
